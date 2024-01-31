d18825164ae838bb43089ccf60990937c3983c1e
Yaddy23 <86038304+Yaddy23@users.noreply.github.com>
2024-01-31 15:27:15 +0800

    Update server.js
    testtest

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ app.use(express.static(path.join(__dirname, './client/build')));
 
 //rest api
 app.use("*", function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  res.sendFile(path.join(__dirname, './client/build/index.html'));
 });
 
 app.get("/", (req, res) => {
