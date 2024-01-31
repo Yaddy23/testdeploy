e3c1bb6c9bd0b945ed556bc5b95f4a6e85288934
Yaddy23 <86038304+Yaddy23@users.noreply.github.com>
2024-01-31 15:36:46 +0800

    Update server.js
    test 2

--- server.js
+++ server.js
@@ -33,11 +33,12 @@ app.use(morgan("dev"));
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/category", categoryRoutes);
 app.use("/api/v1/product", productRoutes);
-app.use(express.static(path.join(__dirname, './client/build')));
+const clientBuildPath = path.join(__dirname, "client", "build");
+app.use(express.static(clientBuildPath));
 
 //rest api
 app.use("*", function (req, res) {
-  res.sendFile(path.join(__dirname, './client/build/index.html'));
+  res.sendFile(path.join(clientBuildPath, "index.html"));
 });
 
 app.get("/", (req, res) => {
